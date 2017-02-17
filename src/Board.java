bd848e256a7ae4895f2fd28529cc49086029210f
enrico gallus <enrico.gallus@gmail.com>
2017-02-17 12:54:03 +0900

    initial commit

---
+++ src/Board.java
@@ -0,0 +1,83 @@
+public class Board {
+    /**
+     * construct a board from an n-by-n array of blocks
+     * (where blocks[i][j] = block in row i, column j)
+     * @param blocks
+     */
+    public Board(int[][] blocks) {
+
+    }
+
+    /**
+     * board dimension n
+     * @return
+     */
+    public int dimension() {
+
+    }
+
+    /**
+     * number of blocks out of place
+     * @return
+     */
+    public int hamming() {
+
+    }
+
+    /**
+     * sum of Manhattan distances between blocks and goal
+     * @return
+     */
+    public int manhattan() {
+
+    }
+
+    /**
+     * is this board the goal board?
+     * @return
+     */
+    public boolean isGoal() {
+
+    }
+
+    /**
+     * a board that is obtained by exchanging any pair of blocks
+     * @return
+     */
+    public Board twin() {
+
+    }
+
+    /**
+     * does this board equal y?
+     * @param y
+     * @return
+     */
+    public boolean equals(Object y) {
+
+    }
+
+    /**
+     * all neighboring boards
+     * @return
+     */
+    public Iterable<Board> neighbors() {
+
+    }
+
+    /**
+     * string representation of this board (in the output format specified below)
+     * @return
+     */
+    public String toString() {
+
+    }
+
+    /**
+     * unit tests (not graded)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
